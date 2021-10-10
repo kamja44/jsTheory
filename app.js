f3721cd704dea2db7360a08d0b4ad2293ff3e991
kamja44 <88872409+kamja44@users.noreply.github.com>
2021-10-10 16:57:28 +0900

    #2-4
    #Default Value

--- app.js
+++ app.js
@@ -8,15 +8,26 @@ const emails = [
 const foundMail = emails.find((item) => true);
 console.log(foundMail);
 const foundMail1 = emails.find((item) => item.includes("@gmail.com"));
-console.log(foundMail1)
+console.log(foundMail1);
 
-const noGmail = emails.filter(email => !email.includes("@gmail"));
+const noGmail = emails.filter((email) => !email.includes("@gmail"));
 console.log(noGmail);
 
-emails.forEach(email => {
-    console.log(emails.split("@")[0]);
+emails.forEach((email) => {
+  console.log(emails.split("@")[0]);
 });
 
-const cleaned = emails.map(email => email.split("@")[0]);
+const cleaned = emails.map((email) => email.split("@")[0]);
 
-const cleaned1 = emails.map((email, index) => username: email.split("@")[0], index)
+const cleaned1 = emails.map((email, index) => ({
+  username: email.split("@")[0],
+  index,
+}));
+
+// default value
+function sayHi(Name = "missing") {
+  return "Hello" + Name;
+}
+console.log(sayHi());
+
+const sayHiArrow = (Name = "missing") => "Hello" + Name;
