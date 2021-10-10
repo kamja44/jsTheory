60b6a8e89a2c1b7fdf075ad393cd5b04421a38e3
kamja44 <88872409+kamja44@users.noreply.github.com>
2021-10-10 16:49:54 +0900

    arrow function
    # arrow function

---
+++ app.js
@@ -0,0 +1,22 @@
+const emails = [
+  "kamja@naver.com",
+  "naver@google.com",
+  "kkamja@gmail.com",
+  "kkkamja@hanmail.net",
+];
+
+const foundMail = emails.find((item) => true);
+console.log(foundMail);
+const foundMail1 = emails.find((item) => item.includes("@gmail.com"));
+console.log(foundMail1)
+
+const noGmail = emails.filter(email => !email.includes("@gmail"));
+console.log(noGmail);
+
+emails.forEach(email => {
+    console.log(emails.split("@")[0]);
+});
+
+const cleaned = emails.map(email => email.split("@")[0]);
+
+const cleaned1 = emails.map((email, index) => username: email.split("@")[0], index)
