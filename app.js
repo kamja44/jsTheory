5e6e11910786b118ab504c0fbc7454e764e24a87
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-06 23:31:52 +0900

    4.0
    array를 만들 때 -> array.of
    
    array-like object를 array로 변환할 때 ->
    array.from(array-like object)

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const isEmail = (email) => email.includes("@");
-console.log(isEmail("kamja@kamja.kamja"));
+// const friends = ["nico","lynn","dal"]
 
-const CC_NUMBER = "4444";
+// line1과 같은 결과
+const friends = Array.of("nico", "lynn", "dal");
 
-const displayName = `${"*".repeat(10)}${CC_NUMBER}`;
-console.log(displayName);
+console.log(friends);
 
-const name = "Mr.kamja";
-console.log(name.startsWith("Mr"));
-console.log(name.endsWith("Mr"));
+// line 10, line 11 -> array-like object
+// array 같지만 array는 아닌 것
+// const buttons = document.querySelectorAll("button");
+const buttons = document.getElementsByClassName("btn");
+console.log(buttons);
+
+// array가 아닌 이유 -> button은 foreach method가 없다.
+// buttons.foreach((button) => {
+//   button.addEventListener("click", () => console.log(`I ve been click`));
+// });
+
+// Array.from -> creates an array from an array-like object
+// Array.from(array-like object)가 array를 반환
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", () => console.log("Hello"));
+});
+
+// const ar = Array.from(buttons);
+
+// ar.forEach((button) => {
+//   button.addEventListener("click", () => console.log(`hi`));
+// });
