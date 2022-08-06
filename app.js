9820c55dd22ba28e46de1384e29e09073d17ebf5
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-06 19:20:41 +0900

    3.1
    Template Literal은 space 해석이 가능하다.

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-const add = (a, b) => a + b;
-const name = `kong`;
+const wrapper = document.querySelector(".wrapper");
 
-console.log(`a + b = ${add(1, 2)}`);
-console.log(`king${name}`);
+const addWelcome = () => {
+  // const div = document.createElement("div");
+  // const h1 = document.createElement("h1");
+  // h1.innerText = "Hello";
+  // h1.className = "sexyTitle";
+  // div.append(h1);
+  // wrapper.append(div);
+
+  // 위의 코드와 동일한 결과(html fragments)
+  const div = `
+  <div class="hello">
+    <h1 class="title">Hello</h1>
+  </div>
+  `;
+  wrapper.innerHTML = div;
+};
+
+setTimeout(addWelcome, 2000);
+console.log(`
+
+  Hi
+
+
+`);
