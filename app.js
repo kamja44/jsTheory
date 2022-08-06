20db05e68373f7ed8ca913688d9c2a35c8fd4d9c
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-07 00:42:12 +0900

    4.1
    Array.prototype.find()
    Array.prototype.findIndex()
    Array.prototype.fill()
    Array.includes

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
-// const friends = ["nico","lynn","dal"]
+const friends = [
+  "nico@gmail.com",
+  "lynn@naver.com",
+  "dal@yahoo.com",
+  "mark@hanmail.com",
+  "flynn@korea.com,",
+];
 
-// line1과 같은 결과
-const friends = Array.of("nico", "lynn", "dal");
+// @korea.com 메일 찾기
+// @korea.com을 찾은 후 가장 첫번째 요소를 반환
+let target = friends.find((friend) => friend.includes("@korea.com"));
+console.log(target);
 
+// @korea.com의 index 반환
+const target2 = friends.findIndex((friend) => friend.includes("@korea.com"));
+console.log(target2);
+const username = friends[target2].split("@")[0];
+const email = friends[target2].split("@")[1];
+console.log(`${username}@${email}`);
+
+// korea.com을 찾지 못했다면 -1반환
+const check = () =>
+  friends.findIndex((friend) => friend.includes("@korea.com"));
+target = check();
+
+if (target !== -1) {
+  console.log(target);
+  const username = friends[target].split("@")[0];
+  const email = "google.com";
+  friends[target] = `${username}@${email}`;
+  console.log(friends);
+  // array.fill -> array의 요소를 원하는 값으로 채운다.
+  friends.fill("*".repeat(5), 1, 3);
+
+  target = check();
+}
+console.log(target);
 console.log(friends);
-
-// line 10, line 11 -> array-like object
-// array 같지만 array는 아닌 것
-// const buttons = document.querySelectorAll("button");
-const buttons = document.getElementsByClassName("btn");
-console.log(buttons);
-
-// array가 아닌 이유 -> button은 foreach method가 없다.
-// buttons.foreach((button) => {
-//   button.addEventListener("click", () => console.log(`I ve been click`));
-// });
-
-// Array.from -> creates an array from an array-like object
-// Array.from(array-like object)가 array를 반환
-Array.from(buttons).forEach((button) => {
-  button.addEventListener("click", () => console.log("Hello"));
-});
-
-// const ar = Array.from(buttons);
-
-// ar.forEach((button) => {
-//   button.addEventListener("click", () => console.log(`hi`));
-// });
