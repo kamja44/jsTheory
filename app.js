1de2f277acb9de3c6633e2d473e68ebc052f7f83
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-06 19:53:45 +0900

    3.2 html fragments part Two
    array.join() -> array 안의 string 합치기

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 const wrapper = document.querySelector(".wrapper");
 
-const addWelcome = () => {
-  // const div = document.createElement("div");
-  // const h1 = document.createElement("h1");
-  // h1.innerText = "Hello";
-  // h1.className = "sexyTitle";
-  // div.append(h1);
-  // wrapper.append(div);
+const friends = ["me", "lynn", "dal", "mark"];
 
-  // 위의 코드와 동일한 결과(html fragments)
-  const div = `
-  <div class="hello">
-    <h1 class="title">Hello</h1>
-  </div>
-  `;
-  wrapper.innerHTML = div;
-};
+const list = `
+  <h1>People I love</h1>
+  <ul>
+    ${friends.map((friend) => `<li>${friend}</li>`).join("")}
+  </ul>
+`;
+// map은 무엇을 return 하던지 그 값을 array로 반환
 
-setTimeout(addWelcome, 2000);
-console.log(`
-
-  Hi
-
-
-`);
+wrapper.innerHTML = list;
+console.log(friends.join("❤"));
+// friends.join -> 배열의 문자열들 합치기
