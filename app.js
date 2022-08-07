239a9b07e4d43c98fa0fc08a3fbc985573fc2afd
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-07 16:38:24 +0900

    5.3
    function destructuring

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
-const settings = {
-  color: {
-    chosen_color: "dark",
+// function destructuring
+function saveSettings({ notifications, color: { theme } }) {
+  console.log(notifications, theme);
+}
+
+saveSettings({
+  notifications: {
+    follow: true,
+    alert: true,
+    mkt: true,
   },
-};
-
-// const {
-//   color: { chosen_color = "light" },
-// } = settings;
-
-// console.log(chosen_color);
-
-// rename
-// const chosenColor = settings.color.chosen_color || "light";
-
-// rename wiht destructuring
-// const {
-//   color: { chosen_color: chosenColor = "light" },
-// } = settings;
-// console.log(chosenColor);
-
-let chosenColor = "blue";
-console.log(chosenColor);
-({
-  color: { chosen_color: chosenColor = "light" },
-} = settings);
-
-console.log(chosenColor);
-// 새 변수(chosenColor)를 생성하는 대신에 chosenColor를 업데이트하는 방식
+  color: {
+    theme: "blue",
+  },
+});
