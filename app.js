82ad704d2d48e6959bbd96b91b7007b61d68cfc0
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-08 17:37:39 +0900

    8.6
    finally

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-// Promise Race -> Promise중 하나라도 resolve 되거나 reject 되면 된다.
-
-const p1 = new Promise(resolve => {
-    setTimeout(resolve, 10000, "First");
-});
-const p2 = new Promise((resolve, reject) => {
-    setTimeout(reject, 5000, "kamja");
-});
-const p3 = new Promise(resolve => {
-    setTimeout(resolve, 3000, "Third");
-});
-const racePromise = Promise.race([p1,p2,p3]);
-// Promise Race는 가장 빠른 Promise가 반환된다.
-
-racePromise
-  .then(value => console.log(value))
-  .catch(error => console.log(error));
+// finally
+const p1 = new Promise((resolve, reject) => {
+    // setTimeout(resolve, 3000, "First");
+    setTimeout(reject, 3000, "error");
+})
+  .then((value) => console.log(value))
+  .catch(error => console.log(error))
+  .finally(() => console.log("Im done"));
+  // finally <- then이 실행된 후 실행
