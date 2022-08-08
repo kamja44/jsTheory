74b4c9a9fe284ecb33bca74367c4630826c7ef2b
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-08 14:23:02 +0900

    8.1
    creating promise

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const hello = fetch("http://google.com");
+// promise <- 비동기 작업이 맞이할 미래의 완료 또는 실패와 그 결과 값을 나타낸다.
+const kamja = new Promise((resolve, reject) => {
+    // resolve <- error가 없을때의 값
+    // reject <- error가 있을때의 값
+    setTimeout(resolve, 3000, "Yes you are");
+});
 
-console.log("something");
-console.log(hello);
+console.log(kamja);
+setInterval(console.log, 1000, kamja);
