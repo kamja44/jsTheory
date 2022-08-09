7241d43f496dd025fc4ab5683a7e4fc47ecf122e
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-09 14:57:45 +0900

    10.2-1
    how to use Super

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // class => blueprient(청사진)
 class User{
   // constructor <- 생성자
-  constructor(name = "kamja", lastName, email, password){
+  constructor({username, lastName, email, password}){
     // this는 기본적으로 클래스 안에서 볼 수 있고, 클래스 그 자체를 가리킨다.
-    this.username = name;
+    this.username = username;
     this.lastName = lastName;
     this.email = email;
     this.password = password;
@@ -16,35 +16,39 @@ class User{
     console.log(`${this.username} ${this.email}, ${this.password}`);
   }
   updatePassword(newPassword, currentPassword){
-    // 이전 패스워드와 동일하다면 새로운 패스워드로 변경
     if(this.password === currentPassword){
       this.password = newPassword;
-      console.log(`newPassword is ${this.password} it works!`);
+      
     }else{
       console.log("Can't change Password");
     }
   }
 }
 
-const sexyUser = new User("Kamja","Potato","kamja@potato.com",4444); // User class 생성
-// 즉, sexyUser = User class의 instance
-// instance === alive Class
-sexyUser.sayHello();
-
-console.log(sexyUser.password);
-sexyUser.updatePassword("hello", 4444);
-console.log(sexyUser.password);
-
+const sexyUser = new User({
+  username : "Kamja",
+  lastname : "Potato",
+  email : "kamja@potato.com",
+  password : 4444
+});
 // extends Class
 class Admin extends User{
-  constructor(superAdmin){
+  constructor({username, lastName, email, password, superAdmin, isActive}){
+    super({username, lastName, email, password}); // super <- 부모의 클래스 호출 즉, User 클래스 호출
     this.superAdmin = superAdmin;
+    this.isActive = isActive;
   }
   deleteWebsite(){
     console.log("Delete the whole Website");
   }
 }
 
-const sexyAdmin = new Admin("adminKamja","adminPotato","adminkamja@potato.com",4444, true);
-sexyAdmin.deleteWebsite();
-console.log(sexyAdmin.email);
+const admin = new Admin({
+  username : "adminKamja",
+  lastname : "adminPotato",
+  email : "adminkamja@potato.com",
+  password : 444444,
+  superAdmin : true,
+  isActive : true,
+})
+// admin 인스턴스가 User 호출 -> 가능 |||| User 인스턴스가 admin 호출 ->불가능
