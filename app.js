e6b374eb4d27deb22264f988f122e5d5257959dd
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-09 15:11:42 +0900

    10.2-2
    create class, function

--- app.js
+++ app.js
@@ -1,54 +1,30 @@
-// class => blueprient(청사진)
-class User{
-  // constructor <- 생성자
-  constructor({username, lastName, email, password}){
-    // this는 기본적으로 클래스 안에서 볼 수 있고, 클래스 그 자체를 가리킨다.
-    this.username = username;
-    this.lastName = lastName;
-    this.email = email;
-    this.password = password;
+class Counter{
+  constructor({initialNumber = 0, counterId, plusBtn, minusBtn}){
+    this.count = initialNumber;
+    this.counter = document.getElementById(counterId);
+    this.plusBtn = document.getElementById(plusBtn);
+    this.minusBtn = document.getElementById(minusBtn);
+    this.addEventListeners();
   }
-  // function
-  sayHello(){
-    console.log(`Hello, I am ${this.username}`);
+  addEventListeners(){
+    this.plusBtn.addEventListener("click", this.increase);
+    this.minusBtn.addEventListener("click", this.decrease);
   }
-  getProfile(){
-    console.log(`${this.username} ${this.email}, ${this.password}`);
+  increase(){
+    this.count = this.count + 1;
+    this.repaintCount();
   }
-  updatePassword(newPassword, currentPassword){
-    if(this.password === currentPassword){
-      this.password = newPassword;
-      
-    }else{
-      console.log("Can't change Password");
-    }
+  decrease(){
+    this.count = this.count - 1;
+    this.repaintCount();
+  }
+  repaintCount(){
+    this.counter.innerText = this.count;
   }
 }
 
-const sexyUser = new User({
-  username : "Kamja",
-  lastname : "Potato",
-  email : "kamja@potato.com",
-  password : 4444
+new Counter({
+  counterId : "count",
+  plusBtn : "add",
+  minusBtn : "minus",
 });
-// extends Class
-class Admin extends User{
-  constructor({username, lastName, email, password, superAdmin, isActive}){
-    super({username, lastName, email, password}); // super <- 부모의 클래스 호출 즉, User 클래스 호출
-    this.superAdmin = superAdmin;
-    this.isActive = isActive;
-  }
-  deleteWebsite(){
-    console.log("Delete the whole Website");
-  }
-}
-
-const admin = new Admin({
-  username : "adminKamja",
-  lastname : "adminPotato",
-  email : "adminkamja@potato.com",
-  password : 444444,
-  superAdmin : true,
-  isActive : true,
-})
-// admin 인스턴스가 User 호출 -> 가능 |||| User 인스턴스가 admin 호출 ->불가능
