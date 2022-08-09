f6c76ff428b149907457e778509ee20e2dc40ea9
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-09 14:32:54 +0900

    10.1
    recap class
    extending class

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 // class => blueprient(청사진)
 class User{
   // constructor <- 생성자
-  constructor(name = "kamja"){
+  constructor(name = "kamja", lastName, email, password){
+    // this는 기본적으로 클래스 안에서 볼 수 있고, 클래스 그 자체를 가리킨다.
     this.username = name;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
   }
   // function
   sayHello(){
     console.log(`Hello, I am ${this.username}`);
   }
+  getProfile(){
+    console.log(`${this.username} ${this.email}, ${this.password}`);
+  }
+  updatePassword(newPassword, currentPassword){
+    // 이전 패스워드와 동일하다면 새로운 패스워드로 변경
+    if(this.password === currentPassword){
+      this.password = newPassword;
+      console.log(`newPassword is ${this.password} it works!`);
+    }else{
+      console.log("Can't change Password");
+    }
+  }
 }
 
-const sexyUser = new User(); // User class 생성
+const sexyUser = new User("Kamja","Potato","kamja@potato.com",4444); // User class 생성
 // 즉, sexyUser = User class의 instance
 // instance === alive Class
-const uglyUser = new User("potato");
-
-
 sexyUser.sayHello();
-uglyUser.sayHello();
 
+console.log(sexyUser.password);
+sexyUser.updatePassword("hello", 4444);
+console.log(sexyUser.password);
 
+// extends Class
+class Admin extends User{
+  constructor(superAdmin){
+    this.superAdmin = superAdmin;
+  }
+  deleteWebsite(){
+    console.log("Delete the whole Website");
+  }
+}
 
-// const baseOjbect = {
-//   username : "Kamja",
-//   sayHello : function(){
-//     console.log("potato kamja");
-//   },
-// }
-
-// const kamjaUser = baseOjbect;
-// kamjaUser.sayHello()
+const sexyAdmin = new Admin("adminKamja","adminPotato","adminkamja@potato.com",4444, true);
+sexyAdmin.deleteWebsite();
+console.log(sexyAdmin.email);
