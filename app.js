5f569a8cb625a0f7c4d76dfbee593abfdffd028c
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-09 11:10:04 +0900

    9.2
    Parallel Async Await(Promise.all)

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+// const response = await fetch("https://yts.mx/api/v2/list_movies.json");
+//     const upcoming = await fetch("https://yts.mx/api/v2/movie_suggestions.json?movie_id=100")
 const getMoviesAsync = async() => {
   try{
-    const response = await fetch("https://yts.mx/api/v2/list_movies.json");
-    // const response = await fetch("https://yts.mx/api/v2/list_movies.json");
-    const json = await response.json();
-    console.log(json);
-    throw Error("Im hungry");
+    const [moviesResponse, suggestionsResponse] = await Promise.all(
+      [fetch("https://yts.mx/api/v2/list_movies.json"), 
+      fetch("https://yts.mx/api/v2/movie_suggestions.json?movie_id=100")]);
+    const [movies, suggestions] = await Promise.all([moviesResponse.json(), suggestionsResponse.json()]);
+    console.log(movies, suggestions);
   } catch(error){
-    console.log(`❌ ${error}`);
+    console.log(`${error}`);
   } finally {
-    console.log(`We are done!`);
+    console.log(`We are Done!`);
   }
   
 }
