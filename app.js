593dd3b641566263dc6d3d8a0f3f3df5950657d8
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-10 10:47:06 +0900

    12.0
    Generators

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-// set <- only Value
-// map <- key with Value
-const map = new Map();
-map.set("age", 20);
-console.log(map);
-// map.entries() <- get iterator
-console.log(map.entries());
-console.log(map.has("age"));
-console.log(map.get("age"));
-// overwrite 가능
-map.set("age","4444");
-console.log(map.get("age"));
+// generator -> 기본적으로 pause 할 수 있는 함수
+function* listPeople(){
+  /// yield <- generator에서 yield는 return과 같은 역할을 한다.
+  yield "kamja";
+  yield "kokumi";
+  yield "potato";
+}
+const listG = listPeople();
+console.log("First",listG.next()); // 첫 번째 yield인 kamja를 value로 반환, done : false <- 반환할 yield가 남아있다.
+console.log("second",listG.next()); // 두 번째 yield인 kokumi를 value로 반환, done : false <- 반환할 yield가 남아있다.
+console.log("Third",listG.next()) // 세 번째 yield인 potato를 value로 반환, done : false <- 반환할 yield가 남아있다.
+console.log("Fourth", listG.next()); // 네 번째 yield인 undefined를 value로 반환, done : true <- 반환할 yield가 없다.
+
+const friends = ["kamja","potato","tomato"];
+function* friendTeller(){
+  for(const friend of friends){
+    yield friend;
+  }
+}
+const friendLooper = friendTeller();
+console.log(friendLooper.next());
