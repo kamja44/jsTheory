6991bc267292ac1992fb6f0334dd8c9892ed406e
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-10 10:06:06 +0900

    11.3
    weakSet

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-// set <- Unique <- value 중복 불가 즉, 중복된 value들은 삭제된다.(하나만 저장된다.)
-
-const sexySet = new Set([1,2,3,4,5,6,7,7,7,7,8]);
-// value는 배열로 할당한다.
-console.log(sexySet);
-
-
-// sexySet에 9라는 value가 있는지 확인
-console.log(sexySet.has(9));
-sexySet.delete(1);
-console.log(sexySet);
-sexySet.clear();
-console.log(sexySet);
-sexySet.add("Hi");
-console.log(sexySet);
-console.log(sexySet.size);
+// weakSet <- object만 저장 가능
+const weakSet = new WeakSet();
+const sexy = {
+  im : true
+};
+weakSet.add(sexy);
+console.log(weakSet);
+weakSet.add({hello : true}); // weakSet 내부에서 생성된 코드
+// {hello : true} Object는 아무곳에서도 참조하지 않는다. -> garbage Collector의 정리 대상
