47ef44d72865c6a27dd10256beb43505569aa618
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-10 16:12:48 +0900

    13.1
    ?? Operator

--- app.js
+++ app.js
@@ -1,30 +1,6 @@
-// Proxy
-const userObj = {
-  username : "kamja",
-  age : 20,
-  password : 1234,
-}
-const userFilter = {
-  get : (target, prop, receve) => {
-    // console.log("target",target); // target <- real Object
-    // console.log("prop",prop); // prop <- 내가 요청한 property
-    // console.log("receve",receve); // return proxy
-    // return target[prop];
-    return prop === "password" ? `${"*".repeat(5)}` : target[prop];
-  },
-  set : () => {
-    console.log("Somebody wrote Something");
-  },
-  deleteProperty : (target, prop) => {
-    if(prop === "password"){
-      return "FALSE";
-    }else{
-      target[prop] = "DELETE"
-    }
-  }
-};
+// ?? operator -> 피연산자가 null이거나 undefined일때 작동하는 operator
+let name;
+console.log("Hello", name || "anon");
 
-// userObj의 event를 가로챈 후 userObj의 접근을 막는다.
-const filterUser = new Proxy(userObj, userFilter);
-console.log(filterUser);
-console.log(filterUser.age);
+let name2 =0;
+console.log("Hello", name ?? "kamja");
