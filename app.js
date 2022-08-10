a1ee2a9364e3996f6ca3f01e307a322cf80a6e5e
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-10 09:25:47 +0900

    11.1
    Symbol

--- app.js
+++ app.js
@@ -1,39 +1,26 @@
-class Counter{
-  constructor({initialNumber = 0, counterId, plusBtn, minusBtn}){
-    this.count = initialNumber;
-    this.counter = document.getElementById(counterId);
-    this.counter.innerText = initialNumber;
-    this.plusBtn = document.getElementById(plusBtn);
-    this.minusBtn = document.getElementById(minusBtn);
-    this.addEventListeners();
-  }
-  // eventListener 안에 event의 handler를 넣으면 event target을 가르키는 this를 가지게 된다.(arrow function을 사용하지 않은 경우)
-  // arrow function 사용 시 this는 class를 가리킨다.
-  addEventListeners = () => {
-    this.plusBtn.addEventListener("click", this.increase);
-    this.minusBtn.addEventListener("click", this.decrease);
-  }
-  increase = () => {
-    this.count = this.count + 1;
-    this.repaintCount();
-  }
-  decrease = () => {
-    this.count = this.count - 1;
-    this.repaintCount();
-  }
-  repaintCount = () => {
-    this.counter.innerText = this.count;
-  }
-}
+// Symbol("key") <- key를 Unique로 만든다.
+const hello = Symbol("description");
+const bye = Symbol("description");
 
-new Counter({
-  counterId : "count",
-  plusBtn : "add",
-  minusBtn : "minus",
-});
-new Counter({
-  counterId : "count2",
-  plusBtn : "add2",
-  minusBtn : "minus2",
-  initialNumber : 100,
-})
+const superBig = {
+  [Symbol("kamja")] : {
+  // kamja : {
+    kind : "potato"
+  },
+  [Symbol("kamja")] : {
+    // kamja : {
+      kind : "potato"
+  },
+  hello : "bye",
+}
+// console.log(superBig["kamja"].kind);
+// superBig["kamja"].kind = "kokumi";
+// console.log(superBig["kamja"].kind);
+// Symbol로 key를 Unique로 만들면 값의 수정이 불가능하다.
+console.log(superBig);
+// Symbol로 key를 Unique로 만들면 같은 값도 서로 다르다.
+console.log(Object.keys(superBig));
+console.log(Object.getOwnPropertySymbols(superBig));
+
+const symbol = Object.getOwnPropertySymbols(superBig);
+symbol.forEach(symbol => console.log(superBig[symbol]));
