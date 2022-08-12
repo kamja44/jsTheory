ec6262d735067f2e1c9a648da57adcef88a218cc
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-12 09:44:00 +0900

    13.3
    padStart and padEnd

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const me = {
-    name : "kamja",
-    profile : {
-        email : "something@kamja.co"
-    }
-}
-console.log(me.profile.email);
-console.log(me.potato && me.potato.fried); // error가 아닌 undefined출력
-// optional chaning
-console.log(me?.potato?.email?.fried?.nameeee);
-// 즉, me안에 potato가 없으니 me가 출력
+let hours = 12;
+let minutes = 3;
+let seconds = 2;
+console.log(`${hours}h : ${minutes}m : ${seconds}s`);
+// minutes와 seconds를 2자리 수로 표현
+// padStart, padEnd는 String을 사용하는 함수 -> String(minutes).padStart(2,"0") <- minutes가 1자리일 경우 앞에 0을 붙여 2자리로 만든다.
+console.log(`${hours}h:${String(minutes).padStart(2,"0")}m:${String(seconds).padStart(2,"0")}s`);
+console.log("5".padStart(5,"x"));
+console.log("5".padEnd(5,"x"));
+console.log("1".padStart(2,"0").padEnd(3,"s"));
