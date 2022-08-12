ea08f232f7f8dfa98c56e20a3eb7b7cc2f5f94e7
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-12 09:49:32 +0900

    13.4
    trim

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-let hours = 12;
-let minutes = 3;
-let seconds = 2;
-console.log(`${hours}h : ${minutes}m : ${seconds}s`);
-// minutes와 seconds를 2자리 수로 표현
-// padStart, padEnd는 String을 사용하는 함수 -> String(minutes).padStart(2,"0") <- minutes가 1자리일 경우 앞에 0을 붙여 2자리로 만든다.
-console.log(`${hours}h:${String(minutes).padStart(2,"0")}m:${String(seconds).padStart(2,"0")}s`);
-console.log("5".padStart(5,"x"));
-console.log("5".padEnd(5,"x"));
-console.log("1".padStart(2,"0").padEnd(3,"s"));
+// trim <- 공백 자르기
+console.log("                 hello");
+console.log("                 hello".trimStart());
+console.log("hello                 ");
+console.log("hello                 ".trimEnd());
+console.log("                 hello                ");
+console.log("                 hello                ".trim());
+// 단어 사이 공백 제거 불가
+console.log("                 hel lo                ".trim());
