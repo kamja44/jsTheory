818a38c84b9dcfdb1d2cd9f00e110245d750a4ff
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-12 13:03:55 +0900

    13.8
    Promise.allSettled()

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-let nArray = [1,2,3,4,5,6];
-// flat <- 배열의 요소들을 추출한다.
-let fArray = [1,[2],[[8], [8], [[[8], [8], [[6], [5], [3]]]]]];
-// array.flat("원하는 깊이"); default = 1
-console.log(fArray.flat());
-console.log(fArray.flat(2));
-console.log(fArray.flat(3));
-console.log(fArray.flat(4));
-console.log(fArray.flat(5));
+// Promise.all <- 서로 상관이 있는 Promise들을 동작시킬때 사용
+const p = Promise.all([
+    fetch("https://yts.mx/api/v2/list_movies.json"),
+    fetch(),
+    fetch("https://yts.mx/api/v2/list_movies.json"),
+    fetch("https://yts.mx/api/v2/list_movies.json"),
+])
+.then(response => console.log("success! ",response))
+.catch(error => console.log("error : ", error));
+
+
+// status : rejected <- false, status : fulfilled <- true
+// allSettled <- all처럼 모두 성공하지 않아도 된다. 즉,Promise 동작이 끝나기만 하면 된다.
+// Promise.allSettled <- promise들이 서로 독립적이라 상관없을 때
+const t = Promise.allSettled([
+    fetch("https://yts.mx/api/v2/list_movies.json"),
+    fetch(),
+    fetch("https://yts.mx/api/v2/list_movies.json"),
+    fetch("https://yts.mx/api/v2/list_movies.json"),
+])
+.then(response => console.log("success! ",response))
+.catch(error => console.log("error : ", error));
