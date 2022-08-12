3e50d23f15496c724f240568a121ff0f2395f3eb
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-12 11:18:56 +0900

    13.6
    Array.flat()

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const person = {
-    name : "kamja",
-    age : 20
-}
-// Object의 value만 출력
-console.log(Object.values(person));
-// Object의 Entries <- Entries는 배열의 배열을 return한다. 
-console.log(Object.entries(person)); // Key값의 이름과 값이 배열의 형태로 들어간다.
-console.log(Object.entries(person).forEach(item => console.log(item[0], item[1])));
-
-// Object.fromEntries <- 배열의 배열에서부터 Object를 만든다.
-console.log(Object.fromEntries([["name","kamja"],["age",20],["fried","potato"],["hi",true]]));
-// 즉, 배열의 배열로 Object를 생성한다.  
+let nArray = [1,2,3,4,5,6];
+// flat <- 배열의 요소들을 추출한다.
+let fArray = [1,[2],[[8], [8], [[[8], [8], [[6], [5], [3]]]]]];
+// array.flat("원하는 깊이"); default = 1
+console.log(fArray.flat());
+console.log(fArray.flat(2));
+console.log(fArray.flat(3));
+console.log(fArray.flat(4));
+console.log(fArray.flat(5));
