c125a95b43ec195ee9e9f34f526678bd2e662e25
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-12 11:11:17 +0900

    13.5
    Object.entries
    Object.values
    Object.fromEntries

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-// trim <- 공백 자르기
-console.log("                 hello");
-console.log("                 hello".trimStart());
-console.log("hello                 ");
-console.log("hello                 ".trimEnd());
-console.log("                 hello                ");
-console.log("                 hello                ".trim());
-// 단어 사이 공백 제거 불가
-console.log("                 hel lo                ".trim());
+const person = {
+    name : "kamja",
+    age : 20
+}
+// Object의 value만 출력
+console.log(Object.values(person));
+// Object의 Entries <- Entries는 배열의 배열을 return한다. 
+console.log(Object.entries(person)); // Key값의 이름과 값이 배열의 형태로 들어간다.
+console.log(Object.entries(person).forEach(item => console.log(item[0], item[1])));
+
+// Object.fromEntries <- 배열의 배열에서부터 Object를 만든다.
+console.log(Object.fromEntries([["name","kamja"],["age",20],["fried","potato"],["hi",true]]));
+// 즉, 배열의 배열로 Object를 생성한다.  
